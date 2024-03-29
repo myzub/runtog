c4a5011680baf0040db274066fee79258e352fd4
myzub <zub.m345@gmail.com>
2024-03-29 17:31:48 +0100

    0.0.1 - feat: Spring Initializr project created

---
+++ src/main/java/com/runtog/web/RuntogApplication.java
@@ -0,0 +1,13 @@
+package com.runtog.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuntogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuntogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/runtog/web/RuntogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runtog.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuntogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
